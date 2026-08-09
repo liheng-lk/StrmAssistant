90da699c91d2c584108866dff1be4831956a4bb9
liheng-lk <69590718+liheng-lk@users.noreply.github.com>
2026-08-09 15:56:39 +0800

    fix: import provider lookup types for Douban Assist

--- StrmAssistant/Metadata/DoubanAssistBridge.Fixed.cs
+++ StrmAssistant/Metadata/DoubanAssistBridge.Fixed.cs
@@ -2,6 +2,7 @@ using MediaBrowser.Common.Net;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Providers;
 using MediaBrowser.Model.Entities;
 using MediaBrowser.Model.Serialization;
 using System;
