a2a9ff349f613dbcacb4e1e433c3026e881bf4f7
liheng-lk <69590718+liheng-lk@users.noreply.github.com>
2026-08-12 17:18:24 +0800

    feat(ui): add native settings tab page controller

---
+++ StrmAssistant/UI/NativeSettingsTabPageController.cs
@@ -0,0 +1,32 @@
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Plugins.UI.Views;
+using System;
+using System.Threading.Tasks;
+
+namespace StrmAssistant.UI
+{
+    internal sealed class NativeSettingsTabPageController : NativeSettingsControllerBase
+    {
+        private readonly Func<IPluginUIView> _factory;
+
+        public NativeSettingsTabPageController(string pluginId, string name, string displayName, Func<IPluginUIView> factory)
+            : base(pluginId)
+        {
+            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+            PageInfo = new PluginPageInfo
+            {
+                Name = name,
+                DisplayName = displayName,
+                EnableInMainMenu = false,
+                IsMainConfigPage = false,
+            };
+        }
+
+        public override PluginPageInfo PageInfo { get; }
+
+        public override Task<IPluginUIView> CreateDefaultPageView()
+        {
+            return Task.FromResult(_factory());
+        }
+    }
+}
